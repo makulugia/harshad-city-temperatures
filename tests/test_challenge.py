aaa886cded0f16ead514fb8b1993ce39209e9caa
makulugia <67037573+makulugia@users.noreply.github.com>
2022-06-27 17:33:06 -0500

    fixed unit test asynchronousity

--- tests/test_challenge.py
+++ tests/test_challenge.py
@@ -1,19 +1,21 @@
 import unittest
+from unittest import IsolatedAsyncioTestCase
 import sys
 # append the parent folder
 sys.path.append('./')
 from src.coding_challenge import runReport
 
-class TestChallenge(unittest.TestCase):
-	def test_san(self):
+# class TestChallenge(unittest.TestCase):
+class TestChallenge(IsolatedAsyncioTestCase):
+	async def test_san(self):
 		global rr
-		r1, r2, r3 = runReport('San')
+		r1, r2, r3 = await runReport('San')
 		self.assertTrue(r1.lower().find('san') != -1)
 		self.assertTrue(r2.lower().find('san') != -1)
 		self.assertGreater(len(r3),0)
-	def test_noresult(self):
+	async def test_noresult(self):
 		global rr
-		r1, r2, r3 = runReport('S')
+		r1, r2, r3 = await runReport('S')
 		self.assertTrue(r1.lower().find('na') != -1)
 		self.assertTrue(r2.lower().find('na') != -1)
 		self.assertEqual(len(r3),0)
