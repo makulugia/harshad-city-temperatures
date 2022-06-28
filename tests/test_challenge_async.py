580570fb665a6842a753efc8e31372b5b720c8d7
Maku Lugia <austex@gmx.com>
2022-06-28 10:09:43 -0500

    recovered no_result test

--- tests/test_challenge_async.py
+++ tests/test_challenge_async.py
@@ -11,3 +11,10 @@ async def test_challenge_async():
     assert r1.lower().find('san') != -1
     assert r2.lower().find('san') != -1
     assert len(r3)>0
+
+@pytest.mark.asyncio
+async def test_challenge_noresult_async():
+    r1, r2, r3 = await runReport('S')
+    assert r1.lower().find('na') != -1
+    assert r2.lower().find('na') != -1
+    assert len(r3) == 0
